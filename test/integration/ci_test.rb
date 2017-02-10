a2722ee331d50293147f2745bac65ef78c2b0b7d
Colin Walker <colinwalker270@gmail.com>
2017-02-09 19:29:28 -0800

    change test strucutre

--- test/integration/ci_test.rb
+++ test/integration/ci_test.rb
@@ -1,7 +1,20 @@
 require "test_helper"
 
 describe FortCI do
-  it "can queue jobs" do
+  it "can perform and queue jobs" do
+    project = FortCI::Project.first
+
+    initial = FortCI::Pipeline.count
+    post_as_user "/events", {project_id: project.id, name: 'test'}
+    assert response.ok?
+    assert_equal initial + 1, FortCI::Pipeline.count
+
+    pipeline = FortCI::Pipeline.last
+
+    assert_equal 'PENDING', pipeline.status
+  end
+
+  it "can run a pipeline" do
     project = FortCI::Project.first
 
     initial = FortCI::Pipeline.count
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -23,47 +23,47 @@ test_auth = OmniAuth::AuthHash.new(
 )
 OmniAuth.config.mock_auth[:github] = test_auth
 
-include Rack::Test::Methods
-
 require "test_seeder"
-
 require "fort_ci/helpers/serialization_helper"
-include FortCI::SerializationHelper
-
-def user
-  @test_user ||= FortCI::User.find(email: 'colinwalker270@gmail.com')
-end
-
-def team
-  @test_team ||= FortCI::Team.find(name: 'TestTeam')
-end
-
-def get_as_user(path, opts={}, target_user=user)
-  get path, opts, 'rack.session' => {user_id: target_user.id}
-end
-
-def post_as_user(path, opts={}, target_user=user)
-  post path, opts, 'rack.session' => {user_id: target_user.id}
-end
-
-def patch_as_user(path, opts={}, target_user=user)
-  patch path, opts, 'rack.session' => {user_id: target_user.id}
-end
-
-def response
-  last_response
-end
-
-def response_json
-  symbolize_keys(JSON.parse(last_response.body))
-end
-
-def app
-  FortCI::App
-end
 
 class Minitest::Spec
+  include Rack::Test::Methods
+
   def run(*args, &block)
     Sequel::Model.db.transaction(:rollback=>:always, :auto_savepoint=>true){super}
+  end
+
+  def app
+    FortCI::App
+  end
+
+  include FortCI::SerializationHelper
+
+  def user
+    @test_user ||= FortCI::User.find(email: 'colinwalker270@gmail.com')
+  end
+
+  def team
+    @test_team ||= FortCI::Team.find(name: 'TestTeam')
+  end
+
+  def get_as_user(path, opts={}, target_user=user)
+    get path, opts, 'rack.session' => {user_id: target_user.id}
+  end
+
+  def post_as_user(path, opts={}, target_user=user)
+    post path, opts, 'rack.session' => {user_id: target_user.id}
+  end
+
+  def patch_as_user(path, opts={}, target_user=user)
+    patch path, opts, 'rack.session' => {user_id: target_user.id}
+  end
+
+  def response
+    last_response
+  end
+
+  def response_json
+    symbolize_keys(JSON.parse(last_response.body))
   end
 end

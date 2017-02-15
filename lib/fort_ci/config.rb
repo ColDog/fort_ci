b82b0fd34166bd449e6eeaf2384b3432e636f136
Colin Walker <colinwalker270@gmail.com>
2017-02-15 08:51:15 -0800

    add log sql env var

--- lib/fort_ci/config.rb
+++ lib/fort_ci/config.rb
@@ -13,8 +13,8 @@ module FortCI
       @database = symbolize_keys(load_config_file('./database.yml'))
       @env = (ENV['RAILS_ENV'] || ENV['RACK_ENV'] || ENV['ENV'] || 'development').to_sym
       @secret = ENV['SECRET_KEY_BASE'] || 'secret'
-      @log_sql = false
-      
+      @log_sql = !ENV['LOG_SQL'].nil?
+
       @github_credentials = {
         key: ENV['GITHUB_KEY'],
         secret: ENV['GITHUB_SECRET'],
